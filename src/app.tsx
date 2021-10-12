0e4bb75ea2c8e8e7b6123ffca5d62e7b0b502e01
infantito <danielinfantito61@gmail.com>
2021-10-12 18:54:06 -0500

    feat: update app root

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,9 @@
 import { registerRootComponent } from 'expo'
 import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+
+import { Game, GameOver, StartGame } from '~screens'
+import { Header } from '~components'
 
 const styles = StyleSheet.create({
   screen: {
@@ -9,9 +12,41 @@ const styles = StyleSheet.create({
 })
 
 const App = () => {
+  const [state, setState] = React.useState({
+    userNumber: null as unknown as number,
+    guessRounds: 0,
+  })
+
+  const { userNumber, guessRounds } = state
+
+  const updater = (newState: Partial<typeof state>) => {
+    setState(prevState => ({ ...prevState, ...newState }))
+  }
+
+  const handleRestart = () => {
+    updater({ guessRounds: 0, userNumber: null as unknown as number })
+  }
+
+  const handleStartGame = (selectedNumber: number) => {
+    updater({ userNumber: selectedNumber })
+  }
+
+  const handleGameOver = (numOfRounds: number) => {
+    updater({ guessRounds: numOfRounds })
+  }
+
+  let content = <StartGame handleStartGame={handleStartGame} />
+
+  if (userNumber && guessRounds <= 0) {
+    content = <Game userChoice={userNumber} handleGameOver={handleGameOver} />
+  } else if (guessRounds > 0) {
+    content = <GameOver roundsNumber={guessRounds} userNumber={userNumber} handleRestart={handleRestart} />
+  }
+
   return (
     <View style={styles.screen}>
-      <Text>Guess a Number</Text>
+      <Header title="Guess a Number" />
+      {content}
     </View>
   )
 }
